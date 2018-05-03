3cfebb73d14efbe9abb3071afc4753a44c85c808
rpGIS <rpgiesler@gmail.com>
2018-05-03 09:16:22 -0400

    added POI data

--- ucesrpg/www/js/leaf.js
+++ ucesrpg/www/js/leaf.js
@@ -7,7 +7,7 @@
 function getEarthquakes() {
   client = new XMLHttpRequest();
 
-client.open('GET','https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson');
+client.open('GET','http://developer.cege.ucl.ac.uk:30286/getPOI');
   client.onreadystatechange = earthquakeResponse;
   // note don't use earthquakeResponse() with brackets as that doesn't work
   client.send();
