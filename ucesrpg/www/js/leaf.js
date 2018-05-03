8517ba28831a78253d1c8c1ce00ad78dd71861ad
rpGIS <rpgiesler@gmail.com>
2018-05-03 07:32:04 -0400

    git commit 3

--- ucesrpg/www/js/leaf.js
+++ ucesrpg/www/js/leaf.js
@@ -52,4 +52,26 @@ navigator.geolocation.getCurrentPosition(getPosition);
 function getPosition(position) {
 document.getElementById('showLocation').innerHTML = "Latitude: " + position.coords.latitude +
 "<br>Longitude: " + position.coords.longitude;
-}    
+}
+var xhr; // define the global variable to process the AJAX request
+function callDivChange() {
+xhr = new XMLHttpRequest();
+var filename = document.getElementById("filename").value;
+xhr.open("GET", filename, true);
+xhr.onreadystatechange = processDivChange;
+try {
+ xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+}
+catch (e) {
+}
+xhr.send();
+}
+function processDivChange() {
+if (xhr.readyState < 4) // while waiting response from server
+ document.getElementById('ajaxtest').innerHTML = "Loading...";
+ else if (xhr.readyState === 4) { // 4 = Response from server has been completely loaded.
+ if (xhr.status == 200 && xhr.status < 300)
+// http status between 200 to 299 are all successful
+ document.getElementById('ajaxtest').innerHTML = xhr.responseText;
+ }
+}
